c7b5b5e175a8156ca70a37203ff736d609e4a511
Deborah <119611300+Deborah-Mokeira@users.noreply.github.com>
2023-03-07 09:12:03 -0600

    Display the forecast data using the API.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+//function to get day and timestamp.
 function formatDate(timestamp) {
     let date = new Date(timestamp);
     let hours = date.getHours();
@@ -29,7 +30,7 @@ function formatDate(timestamp) {
   
     return days[day];
   }
-  
+  // function to display weather data
   function displayForecast(response) {
     let forecast = response.data.daily;
   
@@ -38,12 +39,15 @@ function formatDate(timestamp) {
     let forecastHTML = `<div class="row">`;
     forecast.forEach(function (forecastDay, index) {
       if (index < 6) {
-        forecastHTML +=
-                    `
+        forecastHTML =
+          forecastHTML +
+          `
         <div class="col-2">
           <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
           <img
-            src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png"
+            src="http://openweathermap.org/img/wn/${
+              forecastDay.weather[0].icon
+            }@2x.png"
             alt=""
             width="42"
           />
@@ -60,16 +64,17 @@ function formatDate(timestamp) {
       }
     });
   
-    forecastHTML = forecastHTML += `</div>`;
+    forecastHTML = forecastHTML + `</div>`;
     forecastElement.innerHTML = forecastHTML;
   }
-  
+  // function to get weather data by latitude and longitude
   function getForecast(coordinates) {
-    let apiKey = "c0f128b15fc7acb1fb0701090e518e46";
+    console.log(coordinates);
+    let apiKey = "2ff29bed3181c3526c35cc5408037f85";
     let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(displayForecast);
   }
-  
+  // function to display weather data
   function displayTemperature(response) {
     let temperatureElement = document.querySelector("#temperature");
     let cityElement = document.querySelector("#city");
@@ -95,9 +100,9 @@ function formatDate(timestamp) {
   
     getForecast(response.data.coord);
   }
-  
+  // function to get weather data by city name
   function search(city) {
-    let apiKey = "c0f128b15fc7acb1fb0701090e518e46";
+    let apiKey = "2ff29bed3181c3526c35cc5408037f85";
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(displayTemperature);
   }
